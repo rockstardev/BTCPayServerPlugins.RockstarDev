a2361a0f3a34631252e5851096bbef9d687b1b1f
rockstardev <5191402+rockstardev@users.noreply.github.com>
2023-11-24 10:55:29 -0600

    Removing submodule to properly register it

--- submodules/btcpayserver/BTCPayServer/Models/StoreViewModels/TestWebhookViewModel.cs
+++
@@ -1,9 +0,0 @@
-using BTCPayServer.Client.Models;
-
-namespace BTCPayServer.Models.StoreViewModels
-{
-    public class TestWebhookViewModel
-    {
-        public WebhookEventType Type { get; set; }
-    }
-}
--- submodules/btcpayserver/BTCPayServer/Views/UIServer/ServerNavPages.cs
+++
@@ -1,8 +0,0 @@
-namespace BTCPayServer.Views.Server
-{
-    public enum ServerNavPages
-    {
-        Index, Users, Emails, Policies, Theme, Services, Maintenance, Logs, Files, Plugins,
-        Roles
-    }
-}
