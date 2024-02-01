005b6e3e5eb8e93329569ecdacdae0b118d99380
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-02-01 09:57:42 -0600

    Adding hashing of passwords in database

---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/PayrollPluginPassHasher.cs
@@ -0,0 +1,27 @@
+﻿using BTCPayServer.RockstarDev.Plugins.Payroll.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.RockstarDev.Plugins.Payroll
+{
+    public class PayrollPluginPassHasher
+    {
+        private PasswordHasher<string> _hasher = new PasswordHasher<string>();
+
+        public bool IsValidPassword(PayrollUser user, string providedPassword)
+        {
+            var res = _hasher.VerifyHashedPassword(user.Id, user.Password, providedPassword);
+            return res == PasswordVerificationResult.Success || res == PasswordVerificationResult.SuccessRehashNeeded;
+            // TODO: Handle the case of rehashing needed
+        }
+
+        public string HashPassword(string userId, string password)
+        {
+            return _hasher.HashPassword(userId, password);
+        }
+    }
+}
