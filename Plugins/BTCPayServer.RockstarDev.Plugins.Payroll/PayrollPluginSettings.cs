3596bfd9d5f5060ce4f8d09af769e18e26561d00
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-02-01 14:49:54 -0600

    Saving Admin App User Id on first load of grid

---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/PayrollPluginSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.RockstarDev.Plugins.Payroll
+{
+    public class PayrollPluginSettings
+    {
+        public string AdminAppUserId { get; set; }
+    }
+}
