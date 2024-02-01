ee480a5ae948d789ad5cf193cdf0b2f3f703c993
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-02-01 05:46:50 -0600

    Extracting PayrollPluginConst class

---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/PayrollPluginConst.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.RockstarDev.Plugins.Payroll
+{
+    // Need to extract these variables, their references are places for improvements
+    public class PayrollPluginConst
+    {
+        public const string CURRENCY_FIAT = "USD";
+
+        // TODO: Is there a better way here to make it more generic?
+        public const string BTC_CRYPTOCODE = "BTC";
+
+        // TODO: Extrac this outside
+        public const string ADMIN_USER_ID = "79023d5c-7fdf-4277-923d-ca8e37ab2d31";
+    }
+}
