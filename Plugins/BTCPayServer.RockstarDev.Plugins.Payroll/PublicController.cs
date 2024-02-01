9f458fe2e2b811cebaf47547732b103f182e233e
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-02-01 05:47:19 -0600

    Allowing payroll users to upload invoices

--- Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/PublicController.cs
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/PublicController.cs
@@ -1,4 +1,7 @@
-﻿using BTCPayServer.Data;
+﻿using BTCPayServer.Abstractions.Contracts;
+using BTCPayServer.Abstractions.Extensions;
+using BTCPayServer.Abstractions.Models;
+using BTCPayServer.Data;
 using BTCPayServer.Models;
 using BTCPayServer.RockstarDev.Plugins.Payroll.Data;
 using BTCPayServer.RockstarDev.Plugins.Payroll.Data.Models;
@@ -6,7 +9,9 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using NBitcoin;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -23,46 +28,47 @@ public class PublicController : Controller
     private readonly ApplicationDbContextFactory _dbContextFactory;
     private readonly PayrollPluginDbContextFactory _payrollPluginDbContextFactory;
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly BTCPayNetworkProvider _networkProvider;
+    private readonly IFileService _fileService;
 
     public PublicController(ApplicationDbContextFactory dbContextFactory,
         PayrollPluginDbContextFactory payrollPluginDbContextFactory,
-        IHttpContextAccessor httpContextAccessor)
+        IHttpContextAccessor httpContextAccessor,
+        BTCPayNetworkProvider networkProvider,
+        IFileService fileService)
     {
         _dbContextFactory = dbContextFactory;
         _payrollPluginDbContextFactory = payrollPluginDbContextFactory;
         _httpContextAccessor = httpContextAccessor;
+        _networkProvider = networkProvider;
+        _fileService = fileService;
     }
 
 
     [HttpGet("~/plugins/{storeId}/payroll/public/login")]
     public async Task<IActionResult> Login(string storeId)
     {
-        var store = await loadStore(storeId);
-        if (store == null)
-            return NotFound();
+        var vali = await validateStoreAndUser(storeId, false);
+        if (vali.ErrorActionResult != null)
+            return vali.ErrorActionResult;
 
         var model = new PublicLoginViewModel();
-        model.StoreName = store.StoreName;
-        model.StoreBranding = new StoreBrandingViewModel(store.GetStoreBlob());
+        model.StoreName = vali.Store.StoreName;
+        model.StoreBranding = new StoreBrandingViewModel(vali.Store.GetStoreBlob());
 
         return View(model);
-    }
-
-    private async Task<StoreData> loadStore(string storeId)
-    {
-        await using var ctx = _dbContextFactory.CreateContext();
-        return await ctx.Stores.SingleOrDefaultAsync(a => a.Id == storeId);
     }
 
     [HttpPost("~/plugins/{storeId}/payroll/public/login")]
     public async Task<IActionResult> Login(string storeId, PublicLoginViewModel model)
     {
-        var store = await loadStore(storeId);
-        if (store == null)
-            return NotFound();
+        var vali = await validateStoreAndUser(storeId, false);
+        if (vali.ErrorActionResult != null)
+            return vali.ErrorActionResult;
 
-        model.StoreName = store.StoreName;
-        model.StoreBranding = new StoreBrandingViewModel(store.GetStoreBlob());
+        model.StoreId = vali.Store.Id;
+        model.StoreName = vali.Store.StoreName;
+        model.StoreBranding = new StoreBrandingViewModel(vali.Store.GetStoreBlob());
         //
 
         await using var dbPlugins = _payrollPluginDbContextFactory.CreateContext();
@@ -75,24 +81,9 @@ public class PublicController : Controller
             return View(model);
 
         // Validate login credentials here and get user details.
-        _httpContextAccessor.HttpContext.Session.SetString(PAYROLL_AUTH_USER_ID, userInDb.Id);        
+        _httpContextAccessor.HttpContext.Session.SetString(PAYROLL_AUTH_USER_ID, userInDb.Id);
 
         return RedirectToAction(nameof(ListInvoices), new { storeId = storeId });
-    }
-    public class PublicLoginViewModel
-    {
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-        [Display(Name = "Remember me")]
-        public bool RememberMe { get; set; }
-
-        // store properties
-        public string StoreName { get; set; }
-        public StoreBrandingViewModel StoreBranding { get; set; }
     }
 
     private const string PAYROLL_AUTH_USER_ID = "PAYROLL_AUTH_USER_ID";
@@ -114,24 +105,20 @@ public class PublicController : Controller
     [HttpGet("~/plugins/{storeId}/payroll/public/listinvoices")]
     public async Task<IActionResult> ListInvoices(string storeId)
     {
-        var store = await loadStore(storeId);
-        if (store == null)
-            return NotFound();
-
-        var userId = _httpContextAccessor.HttpContext.Session.GetString(PAYROLL_AUTH_USER_ID);
-        if (userId == null)
-            return redirectToLogin(storeId);
+        var vali = await validateStoreAndUser(storeId, true);
+        if (vali.ErrorActionResult != null)
+            return vali.ErrorActionResult;
 
         await using var ctx = _payrollPluginDbContextFactory.CreateContext();
         var payrollInvoices = await ctx.PayrollInvoices
             .Include(data => data.User)
-            .Where(p => p.User.StoreId == storeId && p.UserId == userId && p.IsArchived == false)
+            .Where(p => p.User.StoreId == storeId && p.UserId == vali.UserId && p.IsArchived == false)
             .OrderByDescending(data => data.CreatedAt).ToListAsync();
 
         var model = new PublicListInvoicesViewModel();
-        model.StoreId = store.Id;
-        model.StoreName = store.StoreName;
-        model.StoreBranding = new StoreBrandingViewModel(store.GetStoreBlob());
+        model.StoreId = vali.Store.Id;
+        model.StoreName = vali.Store.StoreName;
+        model.StoreBranding = new StoreBrandingViewModel(vali.Store.GetStoreBlob());
         model.Invoices = payrollInvoices.Select(tuple => new PayrollInvoiceViewModel()
         {
             CreatedAt = tuple.CreatedAt,
@@ -148,13 +135,104 @@ public class PublicController : Controller
 
         return View(model);
     }
-    public class PublicListInvoicesViewModel
-    {
-        public List<PayrollInvoiceViewModel> Invoices { get; set; }
 
-        // store properties
-        public string StoreId { get; set; }
-        public string StoreName { get; set; }
-        public StoreBrandingViewModel StoreBranding { get; set; }
+    private async Task<StoreUserValidator> validateStoreAndUser(string storeId, bool validateUser)
+    {
+        await using var ctx = _dbContextFactory.CreateContext();
+        var store = await ctx.Stores.SingleOrDefaultAsync(a => a.Id == storeId);
+        if (store == null)
+            return new StoreUserValidator { ErrorActionResult = NotFound() };
+
+        string userId = null;
+        if (validateUser)
+        {
+            userId = _httpContextAccessor.HttpContext.Session.GetString(PAYROLL_AUTH_USER_ID);
+            if (userId == null)
+                return new StoreUserValidator { ErrorActionResult = redirectToLogin(storeId) };
+        }
+
+        return new StoreUserValidator { Store = store, UserId = userId };
+    }
+    private class StoreUserValidator
+    {
+        public IActionResult ErrorActionResult { get; set; }
+        public StoreData Store { get; set; }
+        public string UserId { get; set; }
+    }
+
+
+    // upload
+    [HttpGet("~/plugins/{storeId}/payroll/public/upload")]
+    public async Task<IActionResult> Upload(string storeId)
+    {
+        var vali = await validateStoreAndUser(storeId, true);
+        if (vali.ErrorActionResult != null)
+            return vali.ErrorActionResult;
+
+        var model = new PublicPayrollInvoiceUploadViewModel();
+        model.StoreId = vali.Store.Id;
+        model.StoreName = vali.Store.StoreName;
+        model.StoreBranding = new StoreBrandingViewModel(vali.Store.GetStoreBlob());
+
+        model.Currency = vali.Store.GetStoreBlob().DefaultCurrency;
+
+        return View(model);
+    }
+
+    [HttpPost("~/plugins/{storeId}/payroll/upload")]
+
+    public async Task<IActionResult> Upload(string storeId, PublicPayrollInvoiceUploadViewModel model)
+    {
+        var vali = await validateStoreAndUser(storeId, true);
+        if (vali.ErrorActionResult != null)
+            return vali.ErrorActionResult;
+
+        if (model.Amount <= 0)
+        {
+            ModelState.AddModelError(nameof(model.Amount), "Amount must be more than 0.");
+        }
+
+        try
+        {
+            var network = _networkProvider.GetNetwork<BTCPayNetwork>(PayrollPluginConst.BTC_CRYPTOCODE);
+            var address = Network.Parse<BitcoinAddress>(model.Destination, network.NBitcoinNetwork);
+        }
+        catch (Exception)
+        {
+            ModelState.AddModelError(nameof(model.Destination), "Invalid Destination, check format of address.");
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        await using var ctx = _payrollPluginDbContextFactory.CreateContext();
+
+        // TODO: Make saving of the file and entry in the database atomic
+        // TODO: Figure out abstraction of GetAdminUserId()
+        var uploaded = await _fileService.AddFile(model.Invoice, PayrollPluginConst.ADMIN_USER_ID);
+
+        var dbPayrollInvoice = new PayrollInvoice
+        {
+            Amount = model.Amount,
+            CreatedAt = DateTime.UtcNow,
+            Currency = model.Currency,
+            Destination = model.Destination,
+            Description = model.Description,
+            InvoiceFilename = uploaded.Id,
+            UserId = vali.UserId,
+            State = PayrollInvoiceState.AwaitingApproval
+        };
+
+        ctx.Add(dbPayrollInvoice);
+        await ctx.SaveChangesAsync();
+
+        this.TempData.SetStatusMessageModel(new StatusMessageModel()
+        {
+            Message = $"Invoice uploaded successfully",
+            Severity = StatusMessageModel.StatusSeverity.Success
+        });
+        return RedirectToAction(nameof(ListInvoices), new { storeId = storeId });
     }
 }
---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/PublicControllerViewModels.cs
@@ -0,0 +1,54 @@
+﻿using BTCPayServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BTCPayServer.RockstarDev.Plugins.Payroll.PayrollInvoiceController;
+
+namespace BTCPayServer.RockstarDev.Plugins.Payroll;
+
+public class PublicLoginViewModel : BasePayrollPublicViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Display(Name = "Remember me")]
+    public bool RememberMe { get; set; }
+}
+public class PublicListInvoicesViewModel : BasePayrollPublicViewModel
+{
+    public List<PayrollInvoiceViewModel> Invoices { get; set; }
+}
+
+public class PublicPayrollInvoiceUploadViewModel : BasePayrollPublicViewModel
+{
+    [Required]
+    public string Destination { get; set; }
+    [Required]
+    public decimal Amount { get; set; }
+    [Required]
+    public string Currency { get; set; }
+    public string Description { get; set; }
+    [Required]
+    public IFormFile Invoice { get; set; }
+
+
+
+    public SelectList PayrollUsers { get; set; }
+}
+
+public class BasePayrollPublicViewModel
+{
+    // store properties
+    public string StoreId { get; set; }
+    public string StoreName { get; set; }
+    public StoreBrandingViewModel StoreBranding { get; set; }
+}
