056ce60b7e1641e4ea5f65faac1931f05725c96a
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2024-03-14 08:48:05 +0100

    Allow disabling and activating of users

--- Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Data/Migrations/20240314062640_AddingStateToPayrollUser.Designer.cs
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Data/Migrations/20240314062640_AddingStateToPayrollUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BTCPayServer.RockstarDev.Plugins.Payroll.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BTCPayServer.RockstarDev.Plugins.Payroll.Data.Migrations
 {
     [DbContext(typeof(PayrollPluginDbContext))]
-    partial class PayrollPluginDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240314062640_AddingStateToPayrollUser")]
+    partial class AddingStateToPayrollUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -82,7 +85,7 @@ namespace BTCPayServer.RockstarDev.Plugins.Payroll.Data.Migrations
                         .HasColumnType("text");
 
                     b.Property<int>("State")
-                       .HasColumnType("integer");
+                        .HasColumnType("integer");
 
                     b.Property<string>("StoreId")
                         .HasMaxLength(50)
