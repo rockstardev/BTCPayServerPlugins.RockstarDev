a0a975ca9cb4634c7a70be81946835b8e804b2f9
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2024-03-23 09:02:55 +0100

    Default to displaying only Active Users with an option to view All users

---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Data/ViewModels/PayrollUser/PayrollUserListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BTCPayServer.RockstarDev.Plugins.Payroll.Data.Models;
+using System.Collections.Generic;
+
+namespace BTCPayServer.RockstarDev.Plugins.Payroll.Data
+{
+    public class PayrollUserListViewModel
+    {
+        public List<PayrollUser> PayrollUsers { get; set; }
+        public PayrollUserActiveState ActiveState { get; set; }
+    }
+}
---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Data/ViewModels/PayrollUser/PayrollUserResetPasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTCPayServer.RockstarDev.Plugins.Payroll.Data
+{
+    public class PayrollUserResetPasswordViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        [MinLength(6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword", ErrorMessage = "Password fields don't match")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
