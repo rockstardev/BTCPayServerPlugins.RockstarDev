d6508158047b985357263ad4d8cf966d9da2fa7c
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-09-27 15:27:18 -0500

    Creating AdminPassReset plugin

---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.AdminPassReset/Controllers/AdminPassResetController.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using BTCPayServer.Abstractions.Constants;
+using BTCPayServer.Abstractions.Extensions;
+using BTCPayServer.Client;
+using BTCPayServer.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace BTCPayServer.RockstarDev.Plugins.AdminPassReset.Controllers;
+
+[Authorize(Policy = Policies.CanModifyServerSettings, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
+public class AdminPassResetController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly LinkGenerator _generator;
+
+    public AdminPassResetController(UserManager<ApplicationUser> userManager, LinkGenerator generator)
+    {
+        _userManager = userManager;
+        _generator = generator;
+    }
+
+    [HttpGet("~/plugins/adminpassreset/create")]
+    public IActionResult Create()
+    {
+        return View(new AdminPassResetViewModel());
+    }
+
+    [HttpPost("~/plugins/adminpassreset/create")]
+    public async Task<IActionResult> Create(AdminPassResetViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        
+        var user = await _userManager.FindByEmailAsync(model.Email);
+        if (user == null)
+        {
+            ModelState.AddModelError(nameof(model.Email), "User doesn't exist");
+            return View(model);
+        }
+
+        var uri = Request.GetAbsoluteRootUri();
+        var host = new HostString(uri.Host, uri.Port);
+        var code = await _userManager.GeneratePasswordResetTokenAsync(user!);
+        model.CallbackUrl = _generator.ResetPasswordCallbackLink(user.Id, code, uri.Scheme, host, uri.PathAndQuery);
+        return View(model);
+    }
+
+    public class AdminPassResetViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email address of the user")]
+        public string Email { get; set; }
+
+        public string CallbackUrl { get; set; }
+    }
+}
---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.AdminPassReset/Program.cs
@@ -0,0 +1,23 @@
+﻿using BTCPayServer.Abstractions.Contracts;
+using BTCPayServer.Abstractions.Models;
+using BTCPayServer.Abstractions.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BTCPayServer.RockstarDev.Plugins.AdminPassReset
+{
+    public class AdminPassResetPlugin : BaseBTCPayServerPlugin
+    {
+        public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
+        {
+            new() {Identifier = nameof(BTCPayServer), Condition = ">=1.12.0"}
+        };
+
+        public override void Execute(IServiceCollection applicationBuilder)
+        {
+            applicationBuilder.AddSingleton<IUIExtension>(new UIExtension("PluginNav",
+                "store-integrations-nav"));
+            base.Execute(applicationBuilder);
+        }
+    }
+}
