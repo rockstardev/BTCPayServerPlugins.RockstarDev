db7c6d3d6e6efd0b472a55b1a5b5bcd4d5e21b94
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-10-17 22:51:04 -0500

    Adding foundation for XpubExtractor

---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.XpubExtractor/Program.cs
@@ -0,0 +1,20 @@
+﻿using BTCPayServer.Abstractions.Contracts;
+using BTCPayServer.Abstractions.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BTCPayServer.RockstarDev.Plugins.XpubExtractor
+{
+    public class Program : BaseBTCPayServerPlugin
+    {
+        public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
+        {
+            new() {Identifier = nameof(BTCPayServer), Condition = ">=1.12.0"}
+        };
+
+        public override void Execute(IServiceCollection applicationBuilder)
+        {
+            applicationBuilder.AddUIExtension("store-integrations-nav", "XpubExtractorNav");
+            base.Execute(applicationBuilder);
+        }
+    }
+}
---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.XpubExtractor/README.md
@@ -0,0 +1,13 @@
+﻿# Xpub Extractor
+
+This plugin is designed to to easily extract xpub from hardware wallets using BTCPayServer Vault.
+Make sure you have [the latest version of BTCPayServer Vault](https://github.com/btcpayserver/BTCPayServer.Vault/releases) installed on your computer.
+
+## Usage
+TBA
+
+## Contributing to plugin development
+Open issue or pull request on [BTCPayServerPlugins.RockstarDev GitHub](https://github.com/btcpayserver/BTCPayServer.Vault/releases)
+
+## License
+https://github.com/rockstardev/BTCPayServerPlugins.RockstarDev/blob/master/LICENSE
