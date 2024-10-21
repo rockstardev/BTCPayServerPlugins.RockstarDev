35bd91b8dc3f52ab36bca32078b21dc3620f5610
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-10-20 19:41:52 -0500

    Bumping plugin to 2.0

--- Plugins/BTCPayServer.RockstarDev.Plugins.XpubExtractor/Program.cs
+++ Plugins/BTCPayServer.RockstarDev.Plugins.XpubExtractor/Program.cs
@@ -1,6 +1,5 @@
 ﻿using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Abstractions.Models;
-using BTCPayServer.Abstractions.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BTCPayServer.RockstarDev.Plugins.XpubExtractor
@@ -9,15 +8,12 @@ namespace BTCPayServer.RockstarDev.Plugins.XpubExtractor
     {
         public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
         {
-            new() {Identifier = nameof(BTCPayServer), Condition = ">=1.12.0"}
+            new() {Identifier = nameof(BTCPayServer), Condition = ">=2.0.0"}
         };
 
         public override void Execute(IServiceCollection applicationBuilder)
         {
-            // backwards compatibile way to add UI extension, so it works on older versions of BTCPayServer
-            applicationBuilder.AddSingleton<IUIExtension>(new UIExtension("XpubExtractorNav",
-                "store-integrations-nav"));
-            
+            applicationBuilder.AddUIExtension("store-integrations-nav", "XpubExtractorNav");
             base.Execute(applicationBuilder);
         }
     }
