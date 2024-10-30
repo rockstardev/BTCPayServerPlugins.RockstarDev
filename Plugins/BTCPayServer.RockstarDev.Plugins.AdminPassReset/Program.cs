f2af25b3319908fdfd5ef1a95d34ac3bc5053ddc
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-10-29 21:19:42 -0500

    Cleaning up AdminPassReset plugin

--- Plugins/BTCPayServer.RockstarDev.Plugins.AdminPassReset/Program.cs
+++ Plugins/BTCPayServer.RockstarDev.Plugins.AdminPassReset/Program.cs
@@ -1,22 +1,19 @@
 ﻿using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Abstractions.Models;
-using BTCPayServer.Abstractions.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace BTCPayServer.RockstarDev.Plugins.AdminPassReset
+namespace BTCPayServer.RockstarDev.Plugins.AdminPassReset;
+
+public class AdminPassResetPlugin : BaseBTCPayServerPlugin
 {
-    public class AdminPassResetPlugin : BaseBTCPayServerPlugin
+    public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
-        {
-            new() {Identifier = nameof(BTCPayServer), Condition = ">=1.12.0"}
-        };
+        new() {Identifier = nameof(BTCPayServer), Condition = ">=1.12.0"}
+    };
 
-        public override void Execute(IServiceCollection applicationBuilder)
-        {
-            applicationBuilder.AddUIExtension("store-integrations-nav", "PluginNav");
-            base.Execute(applicationBuilder);
-        }
+    public override void Execute(IServiceCollection applicationBuilder)
+    {
+        applicationBuilder.AddUIExtension("store-integrations-nav", "PluginNav");
+        base.Execute(applicationBuilder);
     }
 }
