bef9a9890156bc912be9acc4715ba4b20a63b5c8
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-11-29 15:45:20 -0600

    Completing migration to new policies across controller
    Adding to commit 140160c516b6529c5683c002e40e8f84e821a5cb

--- Plugins/BTCPayServer.RockstarDev.Plugins.XpubExtractor/Controllers/XpubExtractorController.cs
+++ Plugins/BTCPayServer.RockstarDev.Plugins.XpubExtractor/Controllers/XpubExtractorController.cs
@@ -9,7 +9,7 @@ using NBitcoin;
 
 namespace BTCPayServer.RockstarDev.Plugins.XpubExtractor.Controllers;
 
-[Authorize(Policy = Policies.CanModifyServerSettings, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
+[Authorize(Policy = Policies.CanViewInvoices, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
 public class XpubExtractorController : Controller
 {
     public XpubExtractorController()
