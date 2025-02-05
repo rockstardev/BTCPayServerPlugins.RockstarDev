9958622a02b5436c5df5a1ee1867b9b1271a6003
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-02-05 07:25:54 -0800

    Update user invitation flow with feedback

--- Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Controllers/PayrollSettingController.cs
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Controllers/PayrollSettingController.cs
@@ -1,5 +1,4 @@
 ﻿using BTCPayServer.Abstractions.Constants;
-using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Client;
 using BTCPayServer.Data;
 using BTCPayServer.RockstarDev.Plugins.Payroll.Data;
@@ -8,11 +7,10 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using BTCPayServer.RockstarDev.Plugins.Payroll.Logic;
 using BTCPayServer.RockstarDev.Plugins.Payroll.ViewModels;
-using BTCPayServer.Services;
-using System;
 using BTCPayServer.Abstractions.Extensions;
 using BTCPayServer.Services.Mails;
 using Microsoft.AspNetCore.Routing;
+using BTCPayServer.Abstractions.Models;
 
 namespace BTCPayServer.RockstarDev.Plugins.Payroll.Controllers;
 
@@ -24,11 +22,29 @@ public class PayrollSettingController(PayrollPluginDbContextFactory payrollPlugi
     private StoreData CurrentStore => HttpContext.GetStoreData();
 
     private const string DefaultEmailOnInvoicePaidSubject = @"Your invoice has been paid";
+    private const string DefaultUserInviteEmailSubject = @"You are invited to create a Vendor Pay account";
     private const string DefaultEmailOnInvoicePaidBody = @"Hello {Name},
 
 Your invoice submitted on {CreatedAt} has been paid on {PaidAt}.
 
 See all your invoices on: {VendorPayPublicLink}
+
+Thank you,  
+{StoreName}";
+    private const string DefaultUserInviteEmailBody = @"Hello {Name},
+
+You are invited to create an account on {StoreName}'s Vendor Pay portal by visiting the following link:  
+{VendorPayRegisterLink}
+
+Once your account is created and you log in, you will be able to:
+- View your invoices and submit new ones.
+- Click 'Upload Invoice' to add a payable invoice. Fill out the information accurately. By using the Vendor Pay portal, you are solely responsible for providing an accurate Bitcoin address and assume all liability for any incorrect or inaccessible address.
+- Describe what the payment is related to; be as descriptive as possible to avoid delays.
+- Upload the corresponding invoice file.
+
+Payments will be issued in accordance with the terms of the contracted payment and purchase order.
+
+If you have any questions, please reach out to XXXXXX.
 
 Thank you,  
 {StoreName}";
@@ -44,7 +60,8 @@ Thank you,
             PurchaseOrdersRequired = settings.PurchaseOrdersRequired,
             EmailOnInvoicePaid = settings.EmailOnInvoicePaid,
             EmailOnInvoicePaidSubject = settings.EmailOnInvoicePaidSubject ?? DefaultEmailOnInvoicePaidSubject,
-            EmailOnInvoicePaidBody = settings.EmailOnInvoicePaidBody ?? DefaultEmailOnInvoicePaidBody
+            EmailOnInvoicePaidBody = settings.EmailOnInvoicePaidBody ?? DefaultEmailOnInvoicePaidBody,
+            EmailInviteForUsers = settings.EmailInviteForUsers
         };
         var emailSender = await emailSenderFactory.GetEmailSender(storeId);
         ViewData["StoreEmailSettingsConfigured"] = (await emailSender.GetEmailSettings() ?? new EmailSettings()).IsComplete();
@@ -71,10 +88,16 @@ Thank you,
             EmailOnInvoicePaid = model.EmailOnInvoicePaid,
             EmailOnInvoicePaidSubject = model.EmailOnInvoicePaidSubject,
             EmailOnInvoicePaidBody = model.EmailOnInvoicePaidBody,
+            EmailInviteForUsers = model.EmailInviteForUsers,
             VendorPayPublicLink = link
         };
         
         await payrollPluginDbContextFactory.SetSettingAsync(storeId, settings);
+        TempData.SetStatusMessageModel(new StatusMessageModel()
+        {
+            Message = $"Vendor pay settings updated successfully",
+            Severity = StatusMessageModel.StatusSeverity.Success
+        });
         return RedirectToAction(nameof(PayrollInvoiceController.List), "PayrollInvoice", new { storeId = CurrentStore.Id });
 
     }
