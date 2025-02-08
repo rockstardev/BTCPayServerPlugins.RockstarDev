13b1d8f2083af034f71448a573b9d56ca42f74f3
rockstardev <5191402+rockstardev@users.noreply.github.com>
2025-02-07 23:41:00 -0600

    Refactoring Payroll Invoice and adding admin note field

--- Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Data/Migrations/20250208053045_RefactoringPayrollInvoiceMaxLengthAddingNote.Designer.cs
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Data/Migrations/20250208053045_RefactoringPayrollInvoiceMaxLengthAddingNote.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BTCPayServer.RockstarDev.Plugins.Payroll.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BTCPayServer.RockstarDev.Plugins.Payroll.Data.Migrations
 {
     [DbContext(typeof(PayrollPluginDbContext))]
-    partial class PayrollPluginDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250208053045_RefactoringPayrollInvoiceMaxLengthAddingNote")]
+    partial class RefactoringPayrollInvoiceMaxLengthAddingNote
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Data/Models/PayrollInvoice.cs
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Data/Models/PayrollInvoice.cs
@@ -13,21 +13,30 @@ public class PayrollInvoice
     public string Id { get; set; }
 
     [DisplayName("User ID")]
+    [MaxLength(36)] // guid
     public string UserId { get; set; }
     public PayrollUser User { get; set; }
     public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    [MaxLength(100)]
     public string Destination { get; set; }
     public decimal Amount { get; set; }
+    [MaxLength(5)]
     public string Currency { get; set; }
     [MaxLength(20)]
     public string PurchaseOrder { get; set; }
+    [MaxLength(300)]
     public string Description { get; set; }
+    [MaxLength(36)] // guid
     public string InvoiceFilename { get; set; }
     public bool IsArchived { get; set; }
     public PayrollInvoiceState State { get; set; }
+    [MaxLength(100)] 
     public string TxnId { get; set; }
+    [MaxLength(20)] 
     public string BtcPaid { get; set; }
     public DateTimeOffset? PaidAt { get; set; }
+    [MaxLength(500)] 
+    public string AdminNote { get; set; }
 
     internal static void OnModelCreating(ModelBuilder builder)
     {
