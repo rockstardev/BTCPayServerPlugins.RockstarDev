ac2ce8f209315e692d2ace0cad40525fa46d3ae2
rockstardev <5191402+rockstardev@users.noreply.github.com>
2025-12-26 20:41:39 -0600

    Updating navigation in VendorPay to be shown on the left

---
+++ Plugins/BTCPayServer.RockstarDev.Plugins.Payroll/Views/VendorPay/VendorPayNavPages.cs
@@ -0,0 +1,9 @@
+namespace BTCPayServer.RockstarDev.Plugins.VendorPay.Views
+{
+    public enum VendorPayNavPages
+    {
+        VendorPayInvoices,
+        VendorPayUsers,
+        VendorPaySettings
+    }
+}
